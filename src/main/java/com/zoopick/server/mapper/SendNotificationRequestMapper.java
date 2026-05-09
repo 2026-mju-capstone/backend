9b716d84d0e6d62e2b30f35a1c738f681da8609f
lalaalal <bontin0128@gmail.com>
2026-05-09 14:49:31 +0900

    Docs: NotificationPayload 관련된 주석

--- src/main/java/com/zoopick/server/mapper/SendNotificationRequestMapper.java
+++ src/main/java/com/zoopick/server/mapper/SendNotificationRequestMapper.java
@@ -3,6 +3,7 @@ package com.zoopick.server.mapper;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.zoopick.server.dto.notification.SendNotificationRequest;
 import com.zoopick.server.entity.NotificationType;
+import com.zoopick.server.exception.BadRequestException;
 import com.zoopick.server.exception.InternalServerException;
 import com.zoopick.server.service.command.*;
 import lombok.RequiredArgsConstructor;
@@ -25,6 +26,14 @@ public class SendNotificationRequestMapper {
 
     private final ObjectMapper objectMapper;
 
+    /**
+     * {@linkplain SendNotificationRequest}를 {@linkplain SendNotificationCommand}로 변환합니다.
+     *
+     * @param request {@link SendNotificationRequest}
+     * @return {@link SendNotificationCommand}
+     * @throws BadRequestException     payload의 형식이 잘못되었을 때
+     * @throws InternalServerException 지원하지 않는 알림 형식이거나 서버 장애가 발생했을 때
+     */
     public SendNotificationCommand toCommand(SendNotificationRequest request) {
         try {
             NotificationType type = request.getType();
@@ -33,6 +42,8 @@ public class SendNotificationRequestMapper {
             Class<? extends NotificationPayload> payloadType = payloadTypes.get(type);
             NotificationPayload payload = objectMapper.convertValue(request.getPayload(), payloadType);
             return new SendNotificationCommand(request.getTitle(), request.getBody(), payload);
+        } catch (IllegalArgumentException exception) {
+            throw new BadRequestException("잘못된 요청입니다.", request.getPayload() + " is not readable");
         } catch (RuntimeException exception) {
             throw new InternalServerException("Failed to convert payload " + request.getPayload(), exception);
         }
--- src/main/java/com/zoopick/server/service/command/SendNotificationCommand.java
+++ src/main/java/com/zoopick/server/service/command/SendNotificationCommand.java
@@ -1,7 +1,19 @@
 package com.zoopick.server.service.command;
 
+import com.zoopick.server.dto.notification.SendNotificationRequest;
+import com.zoopick.server.mapper.SendNotificationRequestMapper;
+import com.zoopick.server.service.NotificationService;
 import org.jspecify.annotations.NullMarked;
 
+/**
+ * 알림 전송을 위해 사용되는 Command
+ *
+ * @param title   알림 제목
+ * @param body    알림 내용
+ * @param payload {@link NotificationPayload}
+ * @see SendNotificationRequestMapper#toCommand(SendNotificationRequest)
+ * @see NotificationService
+ */
 @NullMarked
 public record SendNotificationCommand(String title, String body, NotificationPayload payload) {
 
