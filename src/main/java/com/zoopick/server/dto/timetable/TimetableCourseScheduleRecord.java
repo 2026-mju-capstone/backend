82241abc46b732b0fcaa964d9257e54401adc02e
lalaalal <bontin0128@gmail.com>
2026-05-10 18:43:24 +0900

    Feat: JsonProperty 제거

--- src/main/java/com/zoopick/server/dto/timetable/TimetableCourseScheduleRecord.java
+++ src/main/java/com/zoopick/server/dto/timetable/TimetableCourseScheduleRecord.java
@@ -1,6 +1,5 @@
 package com.zoopick.server.dto.timetable;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import com.zoopick.server.entity.DayOfWeek;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -12,10 +11,7 @@ import java.time.LocalTime;
 @Setter
 @AllArgsConstructor
 public class TimetableCourseScheduleRecord {
-    @JsonProperty("day_of_week")
     private DayOfWeek dayOfWeek;
-    @JsonProperty("start_time")
     private LocalTime startTime;
-    @JsonProperty("end_time")
     private LocalTime endTime;
 }
